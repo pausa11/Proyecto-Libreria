597364e701f5bec7fb11510932ec0206fc2e53a2
Kevin Cano <kevin.cano@utp.edu.co>
2025-02-17 21:58:48 -0500

    Primera version de despliegue del backend
    Creacion del .env y modificaciones consiguientes y añadir gunicorn. Tambien la creacion de de un render.yaml.
    En wsgi.py:
    Añadimos el directorio del proyecto al PYTHONPATH

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import environ
+
+# Inicializa django-environ
+env = environ.Env(
+    # Establece valores predeterminados y lanza excepciones si faltan variables de entorno
+    DEBUG=(bool, False)
+)
+
+# Lee el archivo .env
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +31,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^mse+5y)f$hrpl4rl&c50oowexws2sztyhg2b84wwm#q58^1le'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Añadir el directorio del proyecto al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
