be06959faccd7f09a46c0870ee41331ac46a2470
Daniel Toro soto <daniel.toro1@utp.edu.co>
2025-03-05 12:18:53 -0500

    se configurar en enrutado por hash para gh-pages

--- libreria-aurora/src/App.js
+++ libreria-aurora/src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/login';
 
 function App() {
   return (
-    <div className="bg-red-100 h-screen flex flex-col items-center justify-center w-full">
-      <header className="bg-gray-800 text-white w-full flex items-center justify-center h-16">
-        <img src={logo} className="h-8" alt="logo" />
-        <h1 className="text-2xl">Librería Aurora</h1>
-      </header>
-
-
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
---
+++ libreria-aurora/src/components/login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+    return(
+        <div className="bg-red-100 h-screen flex flex-col items-center justify-center w-full">
+            <p>login</p>
+        </div>
+    )
+};
+
+export default Login;
