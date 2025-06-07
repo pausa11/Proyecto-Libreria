ddcb687598aae05ec413a2ae6207197e33f1da4f
YersusZ <jhonsebastian.gil@utp.edu.co>
2025-06-06 20:32:35 -0500

    Cambio de permisos para acceder al endpoint de las tiendas

--- backend/apps/tiendas/views.py
+++ backend/apps/tiendas/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Tienda
 from .serializers import TiendaSerializer
 
 class TiendaViewSet(viewsets.ModelViewSet):
     queryset = Tienda.objects.all()
     serializer_class = TiendaSerializer
+    permission_classes = [permissions.AllowAny]  # Permitir acceso público a todas las operaciones
